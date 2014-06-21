bdaa8559740c7fd371d4353d0342e8505ad7419e
Lindsay Macvean <lindsaymacvean@gmail.com>
2014-06-21 14:45:26 -0700

    added images

--- script.js
+++ script.js
@@ -2,9 +2,24 @@
 var d;
 var level = 1;
 var sword = false;
-var ogre = '<img src="ogre.jpg" />';
+var ogre = 'ogre.jpg';
+
 
 $('#wood').html('You are in a dark wood choose left or right');
+function loadCanvas (dataURL) {
+	var canvas = document.createElement("canvas");
+	var ctx = canvas.getContext('2d');
+	canvas.width = 512;
+	canvas.height = 480;
+	document.body.appendChild(canvas);
+
+	var imageObj = new Image();
+	imageObj.onload = function() {
+		ctx.drawImage(this,0,0)
+	};
+	imageObj.src = dataURL;
+}
+loadCanvas('woods.jpg');
 
 //Lets add the keyboard controls now
 $( document ).keydown(function(e) {
@@ -14,7 +29,8 @@ $( document ).keydown(function(e) {
 
 function checkCode() {
 	if (level === 1 && d === 39 && sword === false ) {
-	  	$('#wood').html('you get eaten by an ogre, you failed'+ogre);
+	  	$('#wood').html('you get eaten by an ogre, you failed');
+	  	loadCanvas('ogre');
 	  	setTimeout(
 	  		function () { location.reload(); },
 	  		1000
