8eeff87d4dcf4fb38ee2515aa9bbc5bfafbe9bf0
Lindsay Macvean <lindsaymacvean@gmail.com>
2014-06-21 12:46:24 -0700

    keypresses:

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ alert('hello');
 
 
 prompt('hello tell me your name?');
+
+var d;
+
+document.onkeypress = function () {
+		var key = e.which;
+		//We will add another clause to prevent reverse gear
+		if(key == "37" && d != "right") d = "left";
+		else if(key == "38" && d != "down") d = "up";
+		else if(key == "39" && d != "left") d = "right";
+		else if(key == "40" && d != "up") d = "down";
+		//The snake is now keyboard controllable
+	};
+
