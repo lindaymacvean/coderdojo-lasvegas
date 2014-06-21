430fa228b05c615e4708d216d2001396101de5ba
Lindsay Macvean <lindsaymacvean@gmail.com>
2014-06-21 14:51:45 -0700

    added images

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var sword = false;
 var ogre = 'ogre.jpg';
 var dwarf = 'dwarf.jpg';
 var money = 'money.jpg';
+var woods = 'woods.jpg';
 
 
 
@@ -22,7 +23,7 @@ function loadCanvas (dataURL) {
 	};
 	imageObj.src = dataURL;
 }
-loadCanvas('woods.jpg');
+loadCanvas(woods);
 
 //Lets add the keyboard controls now
 $( document ).keydown(function(e) {
@@ -33,7 +34,7 @@ $( document ).keydown(function(e) {
 function checkCode() {
 	if (level === 1 && d === 39 && sword === false ) {
 	  	$('#wood').html('you get eaten by an ogre, you failed');
-	  	loadCanvas('ogre');
+	  	loadCanvas(ogre);
 	  	setTimeout(
 	  		function () { location.reload(); },
 	  		1000
@@ -42,16 +43,19 @@ function checkCode() {
 	  }
 	else if (level === 1 && d === 39 && sword === true) {
 		$('#wood').html('You slay the ogre and get some money');
+		loadCanvas(money);
 
 	}
-	if (d === 37) {
+	if (d === 37 && level === 1) {
 		level=2;
 		$('#wood').html('you meet a dwarf and he gives you a sword! You can now go back');
+		loadCanvas(dwarf);
 		sword = true;
 
 	}
 	if (d === 40 ) {
 		level=1;
 		$('#wood').html('You are in a dark wood choose left or right');
+		loadCanvas(woods);
 	}
 }
