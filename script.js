ae2adf7fab9975a8122b29cd27174a5fb4acd865
Lindsay Macvean <lindsaymacvean@gmail.com>
2014-06-21 13:04:35 -0700

    added button finally

--- script.js
+++ script.js
@@ -2,17 +2,11 @@
 alert('hello');
 
 
-prompt('hello tell me your name?');
+var d = prompt('hello tell me your name?');
 
-var d;
-
-document.onkeypress = function () {
-		var key = e.which;
-		//We will add another clause to prevent reverse gear
-		if(key == "37" && d != "right") d = "left";
-		else if(key == "38" && d != "down") d = "up";
-		else if(key == "39" && d != "left") d = "right";
-		else if(key == "40" && d != "up") d = "down";
-		//The snake is now keyboard controllable
-	};
+//Lets add the keyboard controls now
+$( document ).keydown(function(e) {
+  alert( "Handler for .keydown() called."+e );
+  console.log(e);
+});
 
