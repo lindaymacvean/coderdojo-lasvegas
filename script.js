3ce4c45440fd6a9a16bf31027d08b1d39a2338dc
Lindsay Macvean <lindsaymacvean@gmail.com>
2014-06-21 13:06:56 -0700

    record the button you pushed

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ var d = prompt('hello tell me your name?');
 
 //Lets add the keyboard controls now
 $( document ).keydown(function(e) {
-  alert( "Handler for .keydown() called."+e );
-  console.log(e);
+  alert( "You just pressed key number: "+e.keyCode );
 });
 
